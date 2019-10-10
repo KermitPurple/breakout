d8a7b1cef0b324ee581d73f553329a4368d912f3
Shane McDonough <shanemcdman@gmail.com>
2019-10-09 17:16:18 -0700

    finished PrintBox function

---
+++ Breakout.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include"../Hide.h"
+#include"../Color.h"
+#include"../gotoxy.h"
+using namespace std;
+
+const int HEIGHT=15,WIDTH=35,rgb[6]={4,6,14,10,11,1};
+int grid[HEIGHT][WIDTH] = {0};//zero in grid means gone 1 means there is still a box there
+
+//////////////TODO:create tick timer////////////////////
+
+
+
+//////////////TODO:create directions////////////////////
+
+
+
+//////////////TODO:Generate arrays////////////////////
+void PrintBox()
+{
+	for(int i = 0;i < HEIGHT;i++)
+	{
+		for(int j=0;j<WIDTH;j++)
+		{
+			color(rgb[(i+j)%6]);
+			cout<< char(219)<<char(219)<<char(219);
+			grid[i][j]=1;
+		}
+		cout << endl;
+	}
+	color(7);
+}
+//////////////TODO:detect colision////////////////////
+
+int main()
+{
+	PrintBox();
+
+
+
+
+	return 0;
+}
